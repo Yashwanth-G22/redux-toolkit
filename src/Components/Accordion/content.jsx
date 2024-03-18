0861a498537b61d3931192fe0671ce2ceb787f80
gownipalli.yeswanth <gownipalli.yeswanth@agilepoint.com>
2024-03-18 16:39:06 +0530

    temp change

--- src/Components/Accordion/content.jsx
+++ src/Components/Accordion/content.jsx
@@ -4,12 +4,12 @@ import { useForm } from "react-hook-form";
 
 export const Content = memo(() => {
     const { watch, handleSubmit, register } = useForm();
-    const onubmit = useCallback((data) => console.log(data), []);
+    const onSubmit = useCallback((data) => console.log(data), []);
 
-    console.log(watch(onubmit))
+    console.log(watch(onSubmit))
 
     return (<>
-        <form onSubmit={handleSubmit(onubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <input name='test' defaultValue='testing' ref={register} />
             <input name='2ndTest' ref={register({ required: true })} />
         </form>
